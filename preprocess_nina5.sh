ab4cfbdf12bfd64ca0baf864d1262213c5b24d40
visionbike <phuc.visionbike@gmail.com>
2022-11-04 02:38:49 +0800

    minor fix

--- preprocess_nina5.sh
+++ preprocess_nina5.sh
@@ -4,5 +4,5 @@ DIR_DATA="${PWD}/datasets/nina5"
 DIR_RAW="${DIR_DATA}/raw"
 DIR_PROC="${DIR_DATA}/processed"
 
-#python preprocess_nina5.py --path "${DIR_RAW}" --save "${DIR_PROC}" --ver 1 --rectify --butter --ssize 5 --wsize 52 --rest
-python preprocess_nina5.py --path "${DIR_RAW}" --save "${DIR_PROC}" --ver 2 --rectify --butter --ssize 5 --wsize 52 --rest
+python preprocess_nina5.py --path "${DIR_RAW}" --save "${DIR_PROC}" --ver 1 --rectify --butter --ssize 5 --wsize 52 --rest
+#python preprocess_nina5.py --path "${DIR_RAW}" --save "${DIR_PROC}" --ver 2 --rectify --butter --ssize 5 --wsize 52 --rest
